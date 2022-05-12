3956bf77b62798b2ef29248cf72b33be425b4383
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-05-12 06:01:29 -0700

    My first commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,15 @@
-nclude <stdlib.h>
 #include "dog.h"
-
-/**
-  * free_dog - ...
-  * @d: ...
-  *
-  * Return: ...
-  */
-void free_dog(dog_t *d)
-{
-	if (d != NULL)
-	{
-		free(d->owner);
-		free(d->name);
-		free(d);
-	}
-}
+##include <stdlib.h>
+#/**
+# * free_dog - function that free dog
+# * @d:pointer
+# */
+# void free_dog(dog_t *d)
+# {
+#   	if (d)
+#   	{
+#   		free(d->name);
+#   		free(d->owner);
+#   		free(d);
+#   	}
+#}
