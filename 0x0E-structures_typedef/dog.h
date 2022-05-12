8381cfadee5c8cee584460c395f57151c2190a7f
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-05-12 06:16:44 -0700

    My first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *  *struct dog - a new type
- *   *@name : string
- *    *@age : float
- *     *@owner : string
- *      *Description: a new type that contains dog name, age, and owner.
- *       */
+ *struct dog - a new type
+ *@name : string
+ *@age : float
+ *@owner : string
+ *Description: a new type that contains dog name, age, and owner.
+ */
 typedef struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
