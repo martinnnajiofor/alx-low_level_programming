1b469eaddc5ce5382fd606648e78464015ffef16
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-05-12 04:08:02 -0700

    My first commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+nclude <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - ...
+  * @d: ...
+  *
+  * Return: ...
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
---
+++ 0x0E-structures_typedef/git_script
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m 'My first commit'
+git push
