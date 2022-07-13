79c9a6b4f551bb98619cb84572081d79d19e9405
Martinnnajifor <martinnnajiofor@yahoo.com>
2022-07-13 13:26:52 -0700

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/martinnnajiofor/alx-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp/ https://raw.github.com/martinnnajiofor/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so
+export LD_PRELOAD=/tmp/liball.so
